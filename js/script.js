3589d640e5ad0c87e435555128e9f4ba1e58d95a
kmalaminstd <alaminkhanstd@gmail.com>
2024-08-28 01:00:36 +0600

    fixed and set responsive home and contact page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-window.addEventListener('scroll', (e)=>{
-    console.log(e);
+const hamburgerBtn = document.querySelector(".hamburger")
+const links = document.querySelector(".nav-links")
+
+
+hamburgerBtn.addEventListener('click', (e)=>{
+    links.classList.toggle("toggle_links");
     
+    if(links.classList.contains("toggle_links")){
+        document.querySelector('body').style.overflow = "hidden";
+    }else{
+        document.querySelector('body').style.overflow = "scroll";
+    }
+    
+    // Prevent the body click event from firing when clicking the hamburger button
+    e.stopPropagation()
+
 })
+
+document.querySelector("body").addEventListener('click', ()=>{
+    if(links.classList.contains("toggle_links")){
+        links.classList.remove("toggle_links")
+        document.querySelector('body').style.overflow = 'scroll'
+    }
+})
+
+
+
